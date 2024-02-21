e95a7181ace0ebd1031c53641d799e99a5a86e75
taehyeon0 <taehyeon0.an@gmail.com>
2024-02-21 20:57:01 +0900

    #1 - 스프링 부트 프로젝트 시작
    스프링 부트 프로젝트 초기화
    * 스프링 웹
    * 데브툴즈
    * 롬복
    * 엑츄에이터

---
+++ src/main/java/than/myboard/MyboardApplication.java
@@ -0,0 +1,13 @@
+package than.myboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyboardApplication.class, args);
+	}
+
+}
