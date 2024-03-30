ee1204f26df1915459fe47b0fd27cdd34cc4ca2f
taehyeon0 <taehyeon0.an@gmail.com>
2024-03-30 13:42:59 +0900

    #21 - 해시태그 검색 페이지를 위한 기능 구현
    해시태그 뷰에 모든 해시태그를 정리해서 보여주는 기능을 넣기 위해,
    해시태그만 추출해서 unique 하게 보여주는 쿼리를 querydsl 로 작성함
    그 외 관련 서비스, 컨트롤러, 테스트 작성함

--- src/main/java/than/myboard/service/ArticleService.java
+++ src/main/java/than/myboard/service/ArticleService.java
@@ -13,6 +13,8 @@ import than.myboard.dto.ArticleDto;
 import than.myboard.dto.ArticleWithCommentsDto;
 import than.myboard.repository.ArticleRepository;
 
+import java.util.List;
+
 @Slf4j
 @RequiredArgsConstructor
 @Transactional
@@ -60,5 +62,17 @@ public class ArticleService {
 
     public void deleteArticle(long articleId) {
         articleRepository.deleteById(articleId);
+    }
+
+    public Page<ArticleDto> searchArticlesViaHashtag(String hashtag, Pageable pageable) {
+        if (hashtag == null || hashtag.isBlank()) {
+            return Page.empty(pageable);
+        }
+
+        return articleRepository.findByHashtag(hashtag, pageable).map(ArticleDto::from);
+    }
+
+    public List<String> getHashtags() {
+        return articleRepository.findAllDistinctHashtags();
     }
 }
